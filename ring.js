823e1dabdbed26fe25b4780c0370b1cec4d4b6a0
Eric <java.fun@gmail.com>
2016-05-23 20:49:12 +0800

    Signed-off-by: Eric <java.fun@gmail.com>

--- ring.js
+++ ring.js
@@ -39,17 +39,15 @@
     };
     
     ext.rgbRaw = function(index, data) {
-        //if(index<0 || index>7) return;
-        //index -= 1
-        console.log("rgbRGB()");
-        rgbData[index] = data;
+        if(index<1) return;
+        index = --index % 8;
+        rgbData[index+1] = data;
         device.write(rgbData.buffer);
     }
     
     ext.rgbRGB = function(index, r, g, b) {
-        //if(index<0||index>7) return;
-        //index -= 1
-        console.log("rgbRGB()");
+        if(index<1) return;
+        index = --index % 8;
         data = Math.ceil(r*8*32/10) + Math.ceil(g*8*4/10) + Math.ceil(b*4/10);
         rgbData[index] = data;
         device.write(rgbData.buffer);
