388d9353f9302152dea87a033ef80a4ba740c014
Eric <java.fun@gmail.com>
2017-02-12 03:18:53 +0800

    sound

--- python_editor/js/py_sound.js
+++ python_editor/js/py_sound.js
@@ -1,6 +1,11 @@
 var $builtinmodule = function(name)
 {
     var mod = {};
+	mod.play = new Sk.builtin.func(function(url){
+        play(Sk.builtin.asnum$(num, url));
+
+        return new Sk.builtin.none;
+    })
     mod.playSound = new Sk.builtin.func(function(sound_num){
         playSound(Sk.builtin.asnum$(sound_num));
 
