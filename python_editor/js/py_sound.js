0c13a4b7f63254179cb47da8981ad6f334b76736
Eric <java.fun@gmail.com>
2017-02-12 02:10:32 +0800

    sound

--- python_editor/js/py_sound.js
+++ python_editor/js/py_sound.js
@@ -6,5 +6,10 @@ var $builtinmodule = function(name)
 
         return new Sk.builtin.none;
     })
+	mod.playSoundFromURL = new Sk.builtin.func(function(url){
+        playSoundFromURL(Sk.builtin.asnum$(url));
+
+        return new Sk.builtin.none;
+    })
     return mod;
 }
